4c05b87b88dc5f1f9302fc69fab546949a8630d8
Juan Vistro <jpmbvistro@gmail.com>
2021-02-16 12:32:26 -0500

    uploading prev built

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.querySelector('button').addEventListener('click',convertHex)
+
+const hexColorRange = 16777216;
+
+function convertHex() {
+  const month = document.querySelector('#input1').value
+  const day = document.querySelector('#input2').value
+  const year = document.querySelector('#input3').value
+  const mdy = Number(`${month}${day}${year}`)
+  const color = ((month*day*year*mdy)%hexColorRange).toString(16).padStart(6,"0")
+  document.querySelector('#output').innerText = `#${color}`
+  document.querySelector('body').style.backgroundColor = `#${color}`
+  document.querySelector('h1').style.color = `#${color}`
+  document.querySelector('button').style.backgroundColor = `#${color}`
+}
